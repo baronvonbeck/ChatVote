7f6279ff7859782f82be6d5af44a0939d664a4f0
Brian Becker <brbecker@ucdavis.edu>
2019-09-07 14:05:15 -0700

    some cleanup

--- public/js/chat.js
+++ public/js/chat.js
@@ -22,33 +22,34 @@ window.onload = () => {
 	var chatroom = $("#chatroom");
 	var feedback = $("#feedback");
 
-	//Emit message
+	// Emit message
 	send_message.click(function(){
-		socket.emit('new_message', {message : message.val()});
+        socket.emit('new_message', {message : message.val()});
+        message.val('');
 	})
 
-	//Listen on new_message
+	// Listen on new_message
 	socket.on("new_message", (data) => {
 		feedback.html('');
-		message.val('');
 		chatroom.append("<p class='message'>" + data.username + ": " + data.message + "</p>");
 	});
 
-	//Emit a username
+	// Emit a username
 	send_username.click(function(){
 		socket.emit('change_username', {username : username.val()});
 	});
 
-	//Emit typing
+	// Emit typing
 	message.bind("keypress", () => {
 		socket.emit('typing');
 	});
 
-	//Listen on typing
+	// Listen on typing
 	socket.on('typing', (data) => {
 		feedback.html("<p><i>" + data.username + " is typing a message..." + "</i></p>");
     });
     
+    // onnect to a specific room
     socket.on("connect", function() {
         socket.emit('chat', chatRoom);
     })
